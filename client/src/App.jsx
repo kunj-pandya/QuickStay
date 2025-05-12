021b9522002c1f43abefb00ac4211e5e83223bb6
kunj-pandya <kunjpandya998@gmail.com>
2025-05-12 16:01:16 +0530

    Add Hero Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
 
 const App = () => {
 
@@ -8,6 +9,11 @@ const App = () => {
   return (
     <div>
      { !IsOwnerPath && <Navbar />}
+     <div className='min-h-[7ovh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+     </div>
     </div>
   )
 }
