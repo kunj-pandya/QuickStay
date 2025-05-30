8ae469bd91fee1681af6ef8fd1ddda4d7e3c404e
kunj-pandya <kunjpandya998@gmail.com>
2025-05-30 17:30:55 +0530

    Add userModel and Setup Clerk

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import "dotenv/config";
 import cors from "cors";
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
+connectDB()
 
 const app = express();
-// app.use(cors())  Enable Cross-Origin Resource Sharing
+app.use(cors())  
+
+// Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
 
 
 app.get('/', (req, res) => res.send("API is working fine."))
